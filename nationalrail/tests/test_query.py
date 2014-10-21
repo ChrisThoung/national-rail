18bc91785ac146875e0796761baeb2703cf44fc1
Chris Thoung <chris.thoung@gmail.com>
2014-10-21 11:14:49 +0100

    Add `setup()` function to test script
    Add `setup()` function to script to test Query class.

--- nationalrail/tests/test_query.py
+++ nationalrail/tests/test_query.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 
 
+from nose.tools import with_setup
+
 import nationalrail.query
+
+
+def setup():
+    global q
+    q = nationalrail.query.Query(None)
+
+
+@with_setup(setup)
+def test_match_station():
+    pass
 
 
 if __name__ == '__main__':
